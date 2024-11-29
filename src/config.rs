0a3cd40d281d2f9aab5eb8890e4023ec7ec21149
Defelo <mail@defelo.de>
2024-11-29 11:30:56 +0100

    tests: add config test and deny unknown fields

--- src/config.rs
+++ src/config.rs
@@ -12,20 +12,29 @@ use crate::{
 };
 
 pub fn load<'a>(config_path: impl Iterator<Item = &'a str>) -> anyhow::Result<Config> {
-    config_path
-        .fold(
-            config::Config::builder().add_source(File::from_str(
-                include_str!("../config.toml"),
-                FileFormat::Toml,
-            )),
-            |builder, file| builder.add_source(File::with_name(file.trim())),
-        )
-        .build()?
-        .try_deserialize()
-        .map_err(Into::into)
+    load_with_defaults(std::iter::empty(), config_path)
+}
+
+fn load_with_defaults<'a>(
+    defaults: impl Iterator<Item = &'a str>,
+    config_path: impl Iterator<Item = &'a str>,
+) -> anyhow::Result<Config> {
+    let mut builder = config::Config::builder();
+
+    for content in defaults.chain([include_str!("../config.toml")]) {
+        let source = File::from_str(content, FileFormat::Toml);
+        builder = builder.add_source(source);
+    }
+
+    for path in config_path {
+        builder = builder.add_source(File::with_name(path.trim()));
+    }
+
+    builder.build()?.try_deserialize().map_err(Into::into)
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
 pub struct Config {
     #[serde(with = "utils::serde::via_string")]
     pub local_timezone: FixedOffset,
@@ -36,6 +45,7 @@ pub struct Config {
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
 pub struct MatrixConfig {
     pub homeserver: String,
     pub store_path: PathBuf,
@@ -45,6 +55,7 @@ pub struct MatrixConfig {
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
 pub struct AocConfig {
     pub session_file: PathBuf,
     pub leaderboard_rows: usize,
@@ -55,18 +66,21 @@ pub struct AocConfig {
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
 pub struct CacheTtlRule {
     pub minutes_after_unlock: i64,
     pub ttl: u64,
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
 pub struct GarygradyConfig {
     pub interval: u64,
     pub max_age: u64,
 }
 
 #[derive(Debug, Clone, Deserialize)]
+#[serde(deny_unknown_fields)]
 pub struct User {
     pub aoc: Option<AocId>,
     pub matrix: Option<OwnedUserId>,
@@ -90,4 +104,25 @@ where
         .collect();
 
     Ok(RegexSetReplacer::new(rules))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn load() {
+        load_with_defaults(
+            [
+                "matrix.homeserver = \"https://matrix.example.com\"",
+                "matrix.store_path = \".store\"",
+                "matrix.admin_ids = []",
+                "matrix.room_id = \"!xoXcjSEJPUfQmzETtS:matrix.example.com\"",
+                "aoc.session_file = \".session\"",
+            ]
+            .into_iter(),
+            [concat!(env!("CARGO_MANIFEST_DIR"), "/users.toml")].into_iter(),
+        )
+        .unwrap();
+    }
 }
